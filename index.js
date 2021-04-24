118786cb7000538cab236a3c7af07c1e7b1cf788
Brady Brown <brownb202@gmail.com>
2021-04-23 19:04:50 -0600

    added createStore funciton with a way for users to retrieve state

---
+++ index.js
@@ -0,0 +1,16 @@
+function createStore() {
+//the store has 4 parts
+// 1. the state
+// 2. get the state
+// 3. listen for changes on state
+// 4. update the state
+
+let state;
+
+const getState = () => state
+
+
+return {
+  getState
+}
+}
