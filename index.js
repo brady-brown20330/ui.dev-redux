ddd4bb37fde7ddb26ad308ed61d1e49453a4eb6e
Brady Brown <brownb202@gmail.com>
2021-04-27 12:30:56 -0600

    added todos funciton that will handle state actions

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+let addTodo = {
+  'type': 'ADD_TODO',
+  'todo': {
+    'id': 0,
+    'name': 'Learn Redux',
+    'complete': false
+  }
+}
+
+let removeTodo = {
+  'type': 'REMOVE_TODO',
+  'id': 0
+}
+
+let toggleTodo = {
+  'type': 'TOGGLE_TODO',
+  'id': 0
+}
+
 function createStore() {
 //the store has 4 parts
 // 1. the state
@@ -5,20 +24,20 @@ function createStore() {
 // 3. listen for changes on state
 // 4. update the state
 
-let state;
-let listners = [];
+  let state;
+  let listners = [];
 
-const getState = () => state
+  const getState = () => state
 
-const subscribe = (listner) => {
-  listners.push(listner)
-  return () => {
-    listners = listners.filter((l) => l !== listner)
+  const subscribe = (listner) => {
+    listners.push(listner)
+    return () => {
+      listners = listners.filter((l) => l !== listner)
+    }
   }
-}
 
-return {
-  getState,
-  subscribe
-}
+  return {
+    getState,
+    subscribe
+  }
 }
