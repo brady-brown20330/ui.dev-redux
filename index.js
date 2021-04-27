19f4a85ab7ca473b4bbe7bd2a47b72105ffe832d
Brady Brown <brownb202@gmail.com>
2021-04-27 16:26:30 -0600

    app now handles goals as well as todos

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ function todos (state = [], action) {
       return state.filter((todo) => todo.id !== action.id)
     case 'TOGGLE_TODO' : 
       return state.map((todo) => todo.id !== action.id ? todo : 
-        Object.assign({}, todo, {complete: !todo.complete}))
-        default : 
-          return state
+        Object.assign({}, todo, {complete: !todo.complete})
+      )
+    default : 
+      return state
+  }
+}
+
+function goals (state = [], action) {
+  switch(action.type) {
+    case 'ADD_GOAL' :
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL' :
+      return state.filter((goal) => goal.id !== action.id)
+    
+    default :
+      return state
   }
 }
 
